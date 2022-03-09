505367574507aeee28f2297d9dfe321808aa5f9c
Sophie Datuashvili <89793249+Datuashvili@users.noreply.github.com>
2022-03-09 15:47:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
-[theme]\n
-primaryColor="#08424f"\n
-backgroundColor="#08424f"\n
-secondaryBackgroundColor="#0d5a6a"\n
-textColor="#ffffff"\n
-font=“sans serif”\n
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS=false\n\
+[theme]\n\
+primaryColor="#08424f"\n\
+backgroundColor="#08424f"\n\
+secondaryBackgroundColor="#0d5a6a"\n\
+textColor="#ffffff"\n\
+font=“sans serif”\n\
+\n\
 " > ~/.streamlit/config.toml
